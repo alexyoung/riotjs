67036b3fa91e5a223b269113981eb23eda779f08
Alex Young <alex@helicoid.net>
2009-10-17 22:40:36 +0100

    added benchmarking to tests

--- riot.js
+++ riot.js
@@ -5,13 +5,43 @@ Riot = {
   current_context: '',
   debug: false,
 
+  Benchmark: {
+    results: [],
+
+    addResult: function(start, end) {
+      this.results.push(end - start);
+    },
+
+    displayResults: function() {
+      var total = 0,
+          seconds = 0;
+      for (i = 0; i < this.results.length; i++) {
+        total += this.results[i];
+      }
+      seconds = total / 1000;
+      return this.results.length + ' benchmarks ran in ' + total + 'ms (' + seconds + ' seconds)';
+    },
+
+    run: function(times, callback) {
+      this.results = [];
+      for (i = 0; i < times; i++) {
+        var start = new Date();
+        callback();
+        var end = new Date();
+        this.addResult(start, end);
+      }
+      return this.displayResults();
+    }
+  },
+
   run: function(tests) {
     var onload = window.onload;
     window.onload = function() {
       if (onload) window.onload();
-      tests();
+      var benchmark = Riot.Benchmark.run(1, tests);
       Riot.display('<hr />');
       Riot.summariseAllResults();
+      Riot.display(benchmark);
     }
   },
 
